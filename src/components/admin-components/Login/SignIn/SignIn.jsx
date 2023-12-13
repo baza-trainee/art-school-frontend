ddcbbf377bfc3a5676fe028798a938eb4d93b837
Dima Telechkun <telechkun93@gmail.com>
2023-12-14 01:03:20 +0200

    useForm hook has been applied

--- src/components/admin-components/Login/SignIn/SignIn.jsx
+++ src/components/admin-components/Login/SignIn/SignIn.jsx
@@ -1,23 +1,44 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import BasicContainerLogin from '../BasicContainerLogin/BasicContainerLogin';
 import Heading from '../Heading/Heading';
 import ButtonSubmit from '../../Buttons/SubmitButton/ButtonSubmit.jsx';
+import styles from './SignIn.module.scss';
 
 const SignIn = () => {
-  const handleClick = () => {
-    console.log('Увійти');
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
   };
+
+  // const handleClick = () => {
+  //   console.log('Увійти');
+  // };
+
   return (
-    // <>
-    //   <div>Hello</div>
     <BasicContainerLogin>
       <Heading title="Увійти в акаунт" />
-      <ButtonSubmit
-        handlerSubmitButton={handleClick}
-        nameButton="Увійти"
-        isActive={true}
-      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="email"
+          ref={register}
+          placeholder="name@company.com"
+          className={styles.emailStyle}
+        />
+        <input
+          name="password"
+          ref={register}
+          placeholder="Введіть 6 символів і більше"
+          className={styles.passwordStyle}
+        />
+        <ButtonSubmit
+          // handlerSubmitButton={handleClick}
+          nameButton="Увійти"
+          isActive={true}
+        />
+      </form>
     </BasicContainerLogin>
-    // </>
   );
 };
 export default SignIn;
